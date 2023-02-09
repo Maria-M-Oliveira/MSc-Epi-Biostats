a132d5bfd7ce59d1031eb7413a9c0641c0f2d52d
Maria <oliveira.maria.miguel@gmail.com>
2023-02-09 17:08:59 +0000

    limpeza parte 2

---
+++ TPC_3.R
@@ -0,0 +1,17 @@
+# Libraries
+
+
+
+#Files
+
+
+# comparar as regioes do Norte e de Lisboa quanto ao numero de medicos
+# metodologias estatisticas inferenciais seriam adequadas
+
+
+
+# diferencas estatisticamente significativas entre os valores medios por concelho do número de medicos e do numero de enfermeiros
+
+
+
+# comparar todas as regioes quanto ao numero de medicos
---
+++ TPC_4.R
@@ -0,0 +1,9 @@
+# Libraries
+
+
+
+#Files
+
+
+
+# Quiquadrado
